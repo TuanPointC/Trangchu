b3b81e28bf7ec033a9791b544b24643d8df92712
Punicode <nguyentuangl96@gmail.com>
2020-04-14 15:05:17 +0700

    load10

--- 1.js
+++ 1.js
@@ -1,12 +1,5 @@
 function bar(){
-    var khoi_right = document.getElementById("right");
-    var noidung_anh=document.getElementById("noidung");
-    if (khoi_right.style.display == "none") {
-        khoi_right.style.display="block";
-        noidung_anh.style.width ="1032px";
-    }
-    else{
-         khoi_right.style.display="none";
-        noidung_anh.style.width = "100%";
-    }
+    var nhan=document.getElementById("menu");
+    if(nhan.style.display=="none")nhan.style.display="block";
+    else nhan.style.display="none";
 }

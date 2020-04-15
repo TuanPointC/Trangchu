4a48d73dcb400f65b94ca3dc2b0980b9366f3d5e
Punicode <nguyentuangl96@gmail.com>
2020-04-15 21:05:56 +0700

    load12

--- 1.js
+++ 1.js
@@ -8,3 +8,13 @@ function stm(){
     if(nhan.style.display=="block")nhan.style.display="none";
     else nhan.style.display="block";
 }
+function blog(){
+    var nhan=document.getElementById("blog");
+    if(nhan.style.display=="block")nhan.style.display="none";
+    else nhan.style.display="block";
+}
+function memory(){
+    var nhan=document.getElementById("memory");
+    if(nhan.style.display=="block")nhan.style.display="none";
+    else nhan.style.display="block";
+}
